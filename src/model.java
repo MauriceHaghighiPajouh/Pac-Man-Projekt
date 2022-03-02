ce553762afe92bb86e5be203006dac4b613224ed
Maurice Haghighi Pajouh <94959873+MauriceHaghighiPajouh@users.noreply.github.com>
2022-03-02 10:03:41 +0100

    maurice #1

--- src/model.java
+++ src/model.java
@@ -11,7 +11,7 @@ public class model extends JPanel implements ActionListener {
     private boolean sterben = false;
 
     private final int BLOCK_GROESSE = 24;
-    private final int ANZAHL_BLOCKS = 30;
+    private final int ANZAHL_BLOCKS = 15;
     private final int BILD_GROESSE = BLOCK_GROESSE * ANZAHL_BLOCKS ;
     private final int MAX_GEISTER = 16 ;
     private final int PACMAN_GESCHWINDIGKEIT = 6 ;
@@ -33,6 +33,46 @@ public class model extends JPanel implements ActionListener {
     private short [] screenData;
     private Timer timer;
 
+    private final int leveData[] = {
+            19, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 22,
+            17, 16, 16, 16, 16, 24, 16, 16, 16, 16, 16, 16, 16, 16, 20,
+            25, 24, 24, 24, 28, 0, 17, 16, 16, 16, 16, 16, 16, 16, 20,
+            0,  0,  0,  0,  0,  0, 17, 16, 16, 16, 16, 16, 16, 16, 20,
+            19, 18, 18, 18, 18, 18, 16, 16, 16, 16, 24, 24, 24, 24, 20,
+            17, 16, 16, 16, 16, 16, 16, 16, 16, 20, 0,  0,  0,   0, 21,
+            17, 16, 16, 16, 16, 16, 16, 16, 16, 20, 0,  0,  0,   0, 21,
+            17, 16, 16, 16, 24, 16, 16, 16, 16, 20, 0,  0,  0,   0, 21,
+            17, 16, 16, 20, 0, 17, 16, 16, 16, 16, 18, 18, 18, 18, 20,
+            17, 24, 24, 28, 0, 25, 24, 24, 16, 16, 16, 16, 16, 16, 20,
+            21, 0,  0,  0,  0,  0,  0,   0, 17, 16, 16, 16, 16, 16, 20,
+            17, 18, 18, 22, 0, 19, 18, 18, 16, 16, 16, 16, 16, 16, 20,
+            17, 16, 16, 20, 0, 17, 16, 16, 16, 16, 16, 16, 16, 16, 20,
+            17, 16, 16, 20, 0, 17, 16, 16, 16, 16, 16, 16, 16, 16, 20,
+            25, 24, 24, 24, 26, 24, 24, 24, 24, 24, 24, 24, 24, 24, 28
+
+
+            /* 0 = blau , 1 = wand links , 2 = wand oben , 4 = wand rechts ,
+            8= wand unten , 16 = weiße punkte
+             */
+
+    };
+
+    public Model () {
+        loadImages(); // bild laden
+        initVariables(); // variablen initalisieren
+        addKeyListener(new TAdapter());
+        setFocusable(true);  // fenster fokus
+        initGame();  // spiel initialisieren
+    }
+    private void loadImages(){
+        down = new ImageIcon("/Users/mauricehaghighi/IdeaProjects/Pac-Man-Projekt/gifs/down.gif").getImage();
+        up = new ImageIcon ("/Users/mauricehaghighi/IdeaProjects/Pac-Man-Projekt/gifs/up.gif").getImage();
+        left = new ImageIcon("/Users/mauricehaghighi/IdeaProjects/Pac-Man-Projekt/gifs/left.gif").getImage();
+        right = new ImageIcon ("/Users/mauricehaghighi/IdeaProjects/Pac-Man-Projekt/gifs/right.gif").getImage();
+        heart = new ImageIcon("/Users/mauricehaghighi/IdeaProjects/Pac-Man-Projekt/gifs/heart.png").getImage();
+        ghost = new ImageIcon("/Users/mauricehaghighi/IdeaProjects/Pac-Man-Projekt/gifs/ghost.gif").getImage();
+
+    }
 
 
 
